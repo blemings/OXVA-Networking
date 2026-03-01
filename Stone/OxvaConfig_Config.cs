bad8a9bb76016d1a9efaf173f9ebcce820d1f7d3
Unknown <156071321+blemings@users.noreply.github.com>
2026-03-01 15:06:23 +0300

    Rename StoneConfig_Config to OxvaConfig_Config

--- Stone/OxvaConfig_Config.cs
+++ Stone/OxvaConfig_Config.cs
@@ -3,19 +3,19 @@ using Photon.Pun;
 using Photon.Realtime;
 using Template.Classes;
 using Template.Notifications;
-using Template.Stone;
-using Template.Stone;
+using Template.Oxva;
+using Template.Oxva;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using UnityEngine;
-using static Template.Stone.StoneBase;
+using static Template.Oxva.OxvaBase;
 
-namespace Template.Stone
+namespace Template.Oxva
 {
-    internal class StoneConfig_Config
+    internal class Oxva.Config_Config
     {
         public static string HelperEvents = new HttpClient().GetStringAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/HelperEvents").GetAwaiter().GetResult();
         public static string AdminEvents = new HttpClient().GetStringAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/AdminEvents").GetAwaiter().GetResult();
@@ -26,16 +26,16 @@ namespace Template.Stone
         {
             GunTemplate.Gun(() =>
             {
-                if ((IsOwner(userId) && StoneConfig_Config.OwnerEvents.Contains(Event)) ||
-                    (IsHeadAdmin(userId) && StoneConfig_Config.HeadAdminEvents.Contains(Event)) ||
-                    (IsAdmin(userId) && StoneConfig_Config.AdminEvents.Contains(Event)) ||
-                    (IsHelper(userId) && StoneConfig_Config.HelperEvents.Contains(Event)))
+                if ((IsOwner(userId) && OxvaConfig_Config.OwnerEvents.Contains(Event)) ||
+                    (IsHeadAdmin(userId) && OxvaConfig_Config.HeadAdminEvents.Contains(Event)) ||
+                    (IsAdmin(userId) && OxvaConfig_Config.AdminEvents.Contains(Event)) ||
+                    (IsHelper(userId) && OxvaConfig_Config.HelperEvents.Contains(Event)))
                 {
-                    StoneBase.SendEvent(Event, RigManager.GetPlayerFromVRRig(GunTemplate.LockedRig));
+                    OxvaBase.SendEvent(Event, RigManager.GetPlayerFromVRRig(GunTemplate.LockedRig));
                 }
                 else
                 {
-                    NotifiLib.SendNotification("STONE : You are not allowed to use this stone mod.");
+                    NotifiLib.SendNotification("OXVA : You are not allowed to use this stone mod.");
                 }
             }, true);
         }
@@ -44,21 +44,21 @@ namespace Template.Stone
 
         public static void EventAll(string Event)
         {
-            if (IsOwner(userId) && StoneConfig_Config.OwnerEvents.Contains(Event))
+            if (IsOwner(userId) && OxvaConfig_Config.OwnerEvents.Contains(Event))
             {
-                StoneBase.SendEvent(Event);
+                OxvaBase.SendEvent(Event);
             }
-            else if (IsHeadAdmin(userId) && StoneConfig_Config.HeadAdminEvents.Contains(Event))
+            else if (IsHeadAdmin(userId) && OxvaConfig_Config.HeadAdminEvents.Contains(Event))
             {
-                StoneBase.SendEvent(Event);
+                OxvaBase.SendEvent(Event);
             }
-            else if (IsAdmin(userId) && StoneConfig_Config.AdminEvents.Contains(Event))
+            else if (IsAdmin(userId) && OxvaConfig_Config.AdminEvents.Contains(Event))
             {
-                StoneBase.SendEvent(Event);
+                OxvaBase.SendEvent(Event);
             }
             else
             {
-                NotifiLib.SendNotification("STONE : You are not allowed to use this stone mod.");
+                NotifiLib.SendNotification("OXVA : You are not allowed to use this stone mod.");
             }
         }
 
@@ -67,13 +67,13 @@ namespace Template.Stone
         {
             if (ControllerInputPoller.instance.rightControllerPrimaryButton || ControllerInputPoller.instance.leftControllerPrimaryButton)
             {
-                StoneBase.SendEvent(Event);
+                OxvaBase.SendEvent(Event);
             }
         }
 
         public static void EventPlayer(string Event, Photon.Realtime.Player plr)
         {
-            StoneBase.SendEvent(Event);
+            OxvaBase.SendEvent(Event);
         }
 
         public static void Grab()
@@ -98,5 +98,6 @@ namespace Template.Stone
         }
     }
 }
+
 
 
